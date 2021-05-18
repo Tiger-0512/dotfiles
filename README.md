838b39f535647b94edb0cf3307a552699927599e
Tiger-0512 <63806957+Tiger-0512@users.noreply.github.com>
2021-05-18 13:26:43 +0900

    Add dotfiles of hammerspoon

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 
 - [Zsh($HOME/.zshrc)](https://github.com/Tiger-0512/dotfiles/blob/main/zsh/.zshrc)
 - [Alacritty($HOME/.config/alacritty/alacritty.yml)](https://github.com/Tiger-0512/dotfiles/blob/main/alacritty/alacritty.yml)
+
+- [hammerspoon(.hammerspoon)]()
---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,185 @@
+hs.hotkey.alertDuration = 0
+hs.hints.showTitleThresh = 0
+hs.window.animationDuration = 0
+
+-- Use the standardized config location, if present
+custom_config = hs.fs.pathToAbsolute(os.getenv("HOME") .. '/.config/hammerspoon/private/config.lua')
+if custom_config then
+    print("Loading custom config")
+    dofile( os.getenv("HOME") .. "/.config/hammerspoon/private/config.lua")
+    privatepath = hs.fs.pathToAbsolute(hs.configdir .. '/private/config.lua')
+    if privatepath then
+        hs.alert("You have config in both .config/hammerspoon and .hammerspoon/private.\nThe .config/hammerspoon one will be used.")
+    end
+else
+    -- otherwise fallback to 'classic' location.
+    if not privatepath then
+        privatepath = hs.fs.pathToAbsolute(hs.configdir .. '/private')
+        -- Create `~/.hammerspoon/private` directory if not exists.
+        hs.fs.mkdir(hs.configdir .. '/private')
+    end
+    privateconf = hs.fs.pathToAbsolute(hs.configdir .. '/private/config.lua')
+    if privateconf then
+        -- Load awesomeconfig file if exists
+        require('private/config')
+    end
+end
+
+hsreload_keys = hsreload_keys or {{"cmd", "shift", "ctrl"}, "R"}
+if string.len(hsreload_keys[2]) > 0 then
+    hs.hotkey.bind(hsreload_keys[1], hsreload_keys[2], "Reload Configuration", function() hs.reload() end)
+end
+
+-- ModalMgr Spoon must be loaded explicitly, because this repository heavily relies upon it.
+hs.loadSpoon("ModalMgr")
+
+-- Define default Spoons which will be loaded later
+if not hspoon_list then
+    hspoon_list = {
+        "resizeM",
+    }
+end
+
+-- Load those Spoons
+for _, v in pairs(hspoon_list) do
+    hs.loadSpoon(v)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Create/Register all kinds of modal keybindings environments.
+----------------------------------------------------------------------------------------------------
+-- Register windowHints (Register a keybinding which is NOT modal environment with modal supervisor)
+hswhints_keys = hswhints_keys or {"alt", "tab"}
+if string.len(hswhints_keys[2]) > 0 then
+    spoon.ModalMgr.supervisor:bind(hswhints_keys[1], hswhints_keys[2], 'Show Window Hints', function()
+        spoon.ModalMgr:deactivateAll()
+        hs.hints.windowHints()
+    end)
+end
+
+----------------------------------------------------------------------------------------------------
+-- Register lock screen
+hslock_keys = hslock_keys or {"alt", "L"}
+if string.len(hslock_keys[2]) > 0 then
+    spoon.ModalMgr.supervisor:bind(hslock_keys[1], hslock_keys[2], "Lock Screen", function()
+        hs.caffeinate.lockScreen()
+    end)
+end
+
+----------------------------------------------------------------------------------------------------
+-- resizeM modal environment
+if spoon.WinWin then
+    spoon.ModalMgr:new("resizeM")
+    local cmodal = spoon.ModalMgr.modal_list["resizeM"]
+    cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'Q', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
+    cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
+    cmodal:bind('', 'A', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
+    cmodal:bind('', 'D', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
+    cmodal:bind('', 'W', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
+    cmodal:bind('', 'S', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
+    cmodal:bind('', 'H', 'Lefthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
+    cmodal:bind('', 'L', 'Righthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
+    cmodal:bind('', 'K', 'Uphalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfup") end)
+    cmodal:bind('', 'J', 'Downhalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfdown") end)
+    cmodal:bind('', 'Y', 'NorthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNW") end)
+    cmodal:bind('', 'O', 'NorthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNE") end)
+    cmodal:bind('', 'U', 'SouthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSW") end)
+    cmodal:bind('', 'I', 'SouthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSE") end)
+    cmodal:bind('', 'F', 'Fullscreen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("fullscreen") end)
+    cmodal:bind('', 'C', 'Center Window', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center") end)
+    cmodal:bind('', '=', 'Stretch Outward', function() spoon.WinWin:moveAndResize("expand") end, nil, function() spoon.WinWin:moveAndResize("expand") end)
+    cmodal:bind('', '-', 'Shrink Inward', function() spoon.WinWin:moveAndResize("shrink") end, nil, function() spoon.WinWin:moveAndResize("shrink") end)
+    cmodal:bind('shift', 'H', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
+    cmodal:bind('shift', 'L', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
+    cmodal:bind('shift', 'K', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
+    cmodal:bind('shift', 'J', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
+    cmodal:bind('', 'left', 'Move to Left Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("left") end)
+    cmodal:bind('', 'right', 'Move to Right Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("right") end)
+    cmodal:bind('', 'up', 'Move to Above Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("up") end)
+    cmodal:bind('', 'down', 'Move to Below Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("down") end)
+    cmodal:bind('', 'space', 'Move to Next Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("next") end)
+    cmodal:bind('', '[', 'Undo Window Manipulation', function() spoon.WinWin:undo() end)
+    cmodal:bind('', ']', 'Redo Window Manipulation', function() spoon.WinWin:redo() end)
+    cmodal:bind('', '`', 'Center Cursor', function() spoon.WinWin:centerCursor() end)
+
+    -- Register resizeM with modal supervisor
+    hsresizeM_keys = hsresizeM_keys or {"alt", "R"}
+    if string.len(hsresizeM_keys[2]) > 0 then
+        spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
+            -- Deactivate some modal environments or not before activating a new one
+            spoon.ModalMgr:deactivateAll()
+            -- Show an status indicator so we know we're in some modal environment now
+            spoon.ModalMgr:activate({"resizeM"}, "#B22222")
+        end)
+    end
+end
+
+----------------------------------------------------------------------------------------------------
+-- Initialize ModalMgr supervisor
+spoon.ModalMgr.supervisor:enter()
+
+
+----------------------------------------------------------------------------------------------------
+-- Open terminal with Second Alt(Option)
+firstAlt = false
+secondAlt = false
+
+local function cancelAlt()
+    firstAlt = false
+    secondAlt = false
+end
+
+local function changeTerminal(event)
+    local c = event:getKeyCode()
+    local f = event:getFlags()
+    if event:getType() == hs.eventtap.event.types.flagsChanged then
+        if f['alt'] then
+            if c == 58 or c == 61 then
+                if firstAlt then
+                    secondAlt = true
+                end
+                firstAlt = true
+                hs.timer.doAfter(0.5, function()
+                    cancelAlt()
+                end)
+                if firstAlt and secondAlt then
+                    cancelAlt()
+                    hs.application.launchOrFocus("/Applications/Alacritty.app")
+                end
+            else
+                cancelAlt()
+            end
+        end
+    end
+end
+opop = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, changeTerminal)
+opop:start()
+
+
+----------------------------------------------------------------------------------------------------
+-- Key map settings
+-- Cited from: http://shtaxxx.hatenablog.com/entry/2017/01/22/141831
+local function keyCode(key, modifiers)
+    modifiers = modifiers or {}
+    return function()
+        hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+        hs.timer.usleep(1000)
+        hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+    end
+end
+
+local function remapKey(modifiers, key, keyCode)
+    hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+remapKey({'ctrl'}, 'k', keyCode('up'))
+remapKey({'ctrl'}, 'j', keyCode('down'))
+remapKey({'ctrl'}, 'h', keyCode('left'))
+remapKey({'ctrl'}, 'l', keyCode('right'))
+
+remapKey({'ctrl'}, 'i', keyCode('left', {'cmd'}))
+remapKey({'ctrl'}, 'a', keyCode('right', {'cmd'}))
+
+
+----------------------------------------------------------------------------------------------------
