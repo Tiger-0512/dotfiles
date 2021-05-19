747748773084a8fa4694658d5118e2d2b4f40360
Tiger-0512 <63806957+Tiger-0512@users.noreply.github.com>
2021-05-19 17:52:49 +0900

    Add components of feature

---
+++ hammerspoon/.hammerspoon/private/config.lua
@@ -0,0 +1,17 @@
+-- Specify Spoons which will be loaded
+hspoon_list = {
+    "WinWin",
+}
+
+
+----------------------------------------------------------------------------------------------------
+-- Those keybindings below could be disabled by setting to {"", ""} or {{}, ""}
+
+-- Window hints keybinding: Focuse to any window you want
+hswhints_keys = {"alt", "tab"}
+
+-- Lock computer's screen
+hslock_keys = {"alt", "L"}
+
+-- resizeM environment keybinding: Windows manipulation
+hsresizeM_keys = {"alt", "R"}
