1df5c13e3b511c2f688b4c0b4fd47f7fa0940b95
Tiger-0512 <63806957+Tiger-0512@users.noreply.github.com>
2021-10-07 03:05:41 +0900

    Add SHFTIT feature to hammerspoon settings

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -34,16 +34,17 @@ end
 hs.loadSpoon("ModalMgr")
 
 -- Define default Spoons which will be loaded later
-if not hspoon_list then
-    hspoon_list = {
-        "resizeM",
-    }
-end
+-- if not hspoon_list then
+--     hspoon_list = {
+--         "resizeM",
+--     }
+-- end
 
 -- Load those Spoons
 for _, v in pairs(hspoon_list) do
     hs.loadSpoon(v)
 end
+
 
 ----------------------------------------------------------------------------------------------------
 -- Create/Register all kinds of modal keybindings environments.
@@ -57,6 +58,7 @@ if string.len(hswhints_keys[2]) > 0 then
     end)
 end
 
+
 ----------------------------------------------------------------------------------------------------
 -- Register lock screen
 hslock_keys = hslock_keys or {"alt", "L"}
@@ -66,54 +68,36 @@ if string.len(hslock_keys[2]) > 0 then
     end)
 end
 
-----------------------------------------------------------------------------------------------------
--- resizeM modal environment
-if spoon.WinWin then
-    spoon.ModalMgr:new("resizeM")
-    local cmodal = spoon.ModalMgr.modal_list["resizeM"]
-    cmodal:bind('', 'escape', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
-    cmodal:bind('', 'Q', 'Deactivate resizeM', function() spoon.ModalMgr:deactivate({"resizeM"}) end)
-    cmodal:bind('', 'tab', 'Toggle Cheatsheet', function() spoon.ModalMgr:toggleCheatsheet() end)
-    cmodal:bind('', 'A', 'Move Leftward', function() spoon.WinWin:stepMove("left") end, nil, function() spoon.WinWin:stepMove("left") end)
-    cmodal:bind('', 'D', 'Move Rightward', function() spoon.WinWin:stepMove("right") end, nil, function() spoon.WinWin:stepMove("right") end)
-    cmodal:bind('', 'W', 'Move Upward', function() spoon.WinWin:stepMove("up") end, nil, function() spoon.WinWin:stepMove("up") end)
-    cmodal:bind('', 'S', 'Move Downward', function() spoon.WinWin:stepMove("down") end, nil, function() spoon.WinWin:stepMove("down") end)
-    cmodal:bind('', 'H', 'Lefthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfleft") end)
-    cmodal:bind('', 'L', 'Righthalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfright") end)
-    cmodal:bind('', 'K', 'Uphalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfup") end)
-    cmodal:bind('', 'J', 'Downhalf of Screen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("halfdown") end)
-    cmodal:bind('', 'Y', 'NorthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNW") end)
-    cmodal:bind('', 'O', 'NorthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerNE") end)
-    cmodal:bind('', 'U', 'SouthWest Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSW") end)
-    cmodal:bind('', 'I', 'SouthEast Corner', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("cornerSE") end)
-    cmodal:bind('', 'F', 'Fullscreen', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("fullscreen") end)
-    cmodal:bind('', 'C', 'Center Window', function() spoon.WinWin:stash() spoon.WinWin:moveAndResize("center") end)
-    cmodal:bind('', '=', 'Stretch Outward', function() spoon.WinWin:moveAndResize("expand") end, nil, function() spoon.WinWin:moveAndResize("expand") end)
-    cmodal:bind('', '-', 'Shrink Inward', function() spoon.WinWin:moveAndResize("shrink") end, nil, function() spoon.WinWin:moveAndResize("shrink") end)
-    cmodal:bind('shift', 'H', 'Move Leftward', function() spoon.WinWin:stepResize("left") end, nil, function() spoon.WinWin:stepResize("left") end)
-    cmodal:bind('shift', 'L', 'Move Rightward', function() spoon.WinWin:stepResize("right") end, nil, function() spoon.WinWin:stepResize("right") end)
-    cmodal:bind('shift', 'K', 'Move Upward', function() spoon.WinWin:stepResize("up") end, nil, function() spoon.WinWin:stepResize("up") end)
-    cmodal:bind('shift', 'J', 'Move Downward', function() spoon.WinWin:stepResize("down") end, nil, function() spoon.WinWin:stepResize("down") end)
-    cmodal:bind('', 'left', 'Move to Left Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("left") end)
-    cmodal:bind('', 'right', 'Move to Right Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("right") end)
-    cmodal:bind('', 'up', 'Move to Above Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("up") end)
-    cmodal:bind('', 'down', 'Move to Below Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("down") end)
-    cmodal:bind('', 'space', 'Move to Next Monitor', function() spoon.WinWin:stash() spoon.WinWin:moveToScreen("next") end)
-    cmodal:bind('', '[', 'Undo Window Manipulation', function() spoon.WinWin:undo() end)
-    cmodal:bind('', ']', 'Redo Window Manipulation', function() spoon.WinWin:redo() end)
-    cmodal:bind('', '`', 'Center Cursor', function() spoon.WinWin:centerCursor() end)
 
-    -- Register resizeM with modal supervisor
-    hsresizeM_keys = hsresizeM_keys or {"alt", "R"}
-    if string.len(hsresizeM_keys[2]) > 0 then
-        spoon.ModalMgr.supervisor:bind(hsresizeM_keys[1], hsresizeM_keys[2], "Enter resizeM Environment", function()
-            -- Deactivate some modal environments or not before activating a new one
-            spoon.ModalMgr:deactivateAll()
-            -- Show an status indicator so we know we're in some modal environment now
-            spoon.ModalMgr:activate({"resizeM"}, "#B22222")
-        end)
-    end
-end
+----------------------------------------------------------------------------------------------------
+units = {
+  right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  right70       = { x = 0.30, y = 0.00, w = 0.70, h = 1.00 },
+  left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
+  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
+  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
+  botright30    = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
+  upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
+  botleft70     = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+}
+
+mash = {'alt', 'shift', 'ctrl'}
+hs.hotkey.bind(mash, 'l', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
+hs.hotkey.bind(mash, 'h', function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
+hs.hotkey.bind(mash, 'k', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
+hs.hotkey.bind(mash, 'j', function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
+hs.hotkey.bind(mash, ']', function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
+hs.hotkey.bind(mash, '[', function() hs.window.focusedWindow():move(units.upleft70,   nil, true) end)
+hs.hotkey.bind(mash, ';', function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
+hs.hotkey.bind(mash, "'", function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
+hs.hotkey.bind(mash, 'f', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
+
+
 
 ----------------------------------------------------------------------------------------------------
 -- Initialize ModalMgr supervisor
