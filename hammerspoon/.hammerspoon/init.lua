634acd0bee94d9ec35f1461935a0a6024b7bb4bb
Taiga Matsunaga <tiger.bass.disney@gmail.com>
2022-06-09 23:44:42 +0900

    feat: new spoons

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -51,32 +51,21 @@ end
 
 ----------------------------------------------------------------------------------------------------
 units = {
-  right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
-  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-  right70       = { x = 0.30, y = 0.00, w = 0.70, h = 1.00 },
-  left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
-  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-  left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
-  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
-  botright30    = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
-  upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
-  botleft70     = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
-  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+    left50 = {x=0.00, y=0.00, w=0.50, h=1.00},
+    right50 = {x=0.50, y=0.00, w=0.50, h=1.00},
+    top50 = {x=0.00, y=0.00, w=1.00, h=0.50},
+    bottom50 = {x=0.00, y=0.50, w=1.00, h=0.50},
+    bottomright = {x=0.40, y=0.50, w=0.60, h=0.50},
+    full = {x=0.00, y=0.00, w=1.00, h=1.00}
 }
 
 mash = {"alt", "shift", "ctrl"}
-hs.hotkey.bind(mash, "l", function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
-hs.hotkey.bind(mash, "h", function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
-hs.hotkey.bind(mash, "k", function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
-hs.hotkey.bind(mash, "j", function() hs.window.focusedWindow():move(units.bot50,      nil, true) end)
--- hs.hotkey.bind(mash, "]", function() hs.window.focusedWindow():move(units.upright30,  nil, true) end)
--- hs.hotkey.bind(mash, "[", function() hs.window.focusedWindow():move(units.upleft70,   nil, true) end)
--- hs.hotkey.bind(mash, ";", function() hs.window.focusedWindow():move(units.botleft70,  nil, true) end)
--- hs.hotkey.bind(mash, """, function() hs.window.focusedWindow():move(units.botright30, nil, true) end)
-hs.hotkey.bind(mash, "f", function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
-
+hs.hotkey.bind(mash, "h", function() hs.window.focusedWindow():move(units.left50, nil, true) end)
+hs.hotkey.bind(mash, "j", function() hs.window.focusedWindow():move(units.bottom50, nil, true) end)
+hs.hotkey.bind(mash, "k", function() hs.window.focusedWindow():move(units.top50, nil, true) end)
+hs.hotkey.bind(mash, "l", function() hs.window.focusedWindow():move(units.right50, nil, true) end)
+hs.hotkey.bind(mash, "b", function() hs.window.focusedWindow():move(units.bottomright, nil, true) end)
+hs.hotkey.bind(mash, "f", function() hs.window.focusedWindow():move(units.full, nil, true) end)
 
 
 ----------------------------------------------------------------------------------------------------
